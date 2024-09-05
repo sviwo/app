89cde7b252e178d20eb1419059399bf2cccce2af
liuWei <332669440@qq.com>
2024-09-05 16:44:19 +0800

    feat: 去除Facebook登录

--- lib/tools/thirdPartLogin/third_part_login_tool.dart
+++ lib/tools/thirdPartLogin/third_part_login_tool.dart
@@ -3,7 +3,7 @@ import 'dart:ffi';
 import 'package:atv/archs/utils/log_util.dart';
 import 'package:atv/widgetLibrary/complex/toast/lw_toast.dart';
 import 'package:basic_utils/basic_utils.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 // import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class ThirdPartLoginTool {
@@ -22,30 +22,31 @@ class ThirdPartLoginTool {
   // }
 
   static Future<Map<String, dynamic>> signInWithFacebook() async {
-    final LoginResult result = await FacebookAuth.instance
-        .login(); // by default we request the email and the public profile
-    var map = <String, dynamic>{};
-    if (result.status == LoginStatus.success) {
-      // you are logged
-      final AccessToken? accessToken = result.accessToken;
+    return {};
+    // final LoginResult result = await FacebookAuth.instance
+    //     .login(); // by default we request the email and the public profile
+    // var map = <String, dynamic>{};
+    // if (result.status == LoginStatus.success) {
+    //   // you are logged
+    //   final AccessToken? accessToken = result.accessToken;
 
-      map['accessToken'] = accessToken?.token ?? '';
+    //   map['accessToken'] = accessToken?.token ?? '';
 
-      map['userId'] = accessToken?.userId ?? '';
+    //   map['userId'] = accessToken?.userId ?? '';
 
-      Map<String, dynamic> userData =
-          await FacebookAuth.instance.getUserData(fields: "name,email");
+    //   Map<String, dynamic> userData =
+    //       await FacebookAuth.instance.getUserData(fields: "name,email");
 
-      LogUtil.d("facebook 获取登录用户信息" + userData.toString());
+    //   LogUtil.d("facebook 获取登录用户信息" + userData.toString());
 
-      map.addAll(userData);
-    } else {
-      LogUtil.d("facebook 登录失败,原因：${result.message}");
-      if (StringUtils.isNotNullOrEmpty(result.message)) {
-        LWToast.show(result.message ?? '');
-      }
-    }
-    return map;
+    //   map.addAll(userData);
+    // } else {
+    //   LogUtil.d("facebook 登录失败,原因：${result.message}");
+    //   if (StringUtils.isNotNullOrEmpty(result.message)) {
+    //     LWToast.show(result.message ?? '');
+    //   }
+    // }
+    // return map;
   }
 }
 // const AuthorizationCredentialAppleID({
